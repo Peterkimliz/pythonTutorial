3bb7daefcebc6e0f72458aadb453dd74d1ef40a0
PeterLojiKim <pkironji@yahoo.com>
2024-03-10 12:22:26 +0300

    string operations

--- data_types.py
+++ data_types.py
@@ -1,7 +1,28 @@
 first_name ="John"
 last_name="Doe"
 age=23
+msg =" how would you like your potatoes to be cooked ?"
 
 # string iterpolation
 print(f" My name is {first_name} {last_name} and iam {age} years  old")
+
+# scapitalize first charachter
+first_name.capitalize
+print(first_name)
+#capitalize all charachters
+first_name.upper()
+print(first_name)
+# getting length of the string 
+print(f" last name has {len(last_name)} charachters")
+
+
+# using range in text
+print(msg[0:])
+
+print(msg[0:5])
+
+print(msg[:-2])
+
+
+
 
