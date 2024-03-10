3ea34ad4c237a6f85d6cf7417a61128400e94fd0
PeterLojiKim <pkironji@yahoo.com>
2024-03-10 12:27:55 +0300

    getting user inputs

---
+++ user_inputs.py
@@ -0,0 +1,5 @@
+first_name=input("Enter your fist name : ")
+last_name=input("Enter your last name: ")
+age =int(input("Enter your age: "))
+
+print(f"Hello {first_name} {last_name} you are {age} years old ")
