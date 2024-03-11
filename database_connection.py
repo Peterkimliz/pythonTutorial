b1cfcd31496526a947eb2f716915f8753e20debe
PeterLojiKim <pkironji@yahoo.com>
2024-03-11 11:24:08 +0300

    added the forloop

--- database_connection.py
+++ database_connection.py
@@ -1,7 +1,7 @@
 import mysql.connector
-mydb = mysql.connector.connect(host="localhost",user="root",password="kimani89",database="laravel")
+mydb = mysql.connector.connect(host="localhost",user="root",password="kimani89",database="python")
 mycursor=mydb.cursor()
-mycursor.execute("select * from categories ORDER BY name asc")
+mycursor.execute("show tables")
 
 for i in mycursor:
     print(i)
---
+++ for_loops.py
@@ -0,0 +1,7 @@
+for i in range(10):
+    print(i)
+
+#inner loop 
+for i in range(3):
+    for j in range(3):
+        print(f"({i},{j})")    
